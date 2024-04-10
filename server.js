63ea3d21c8f39eb7152e4883f6f9eca450b8800b
Sandip040401 <iemsandip@gmail.com>
2024-04-11 01:02:42 +0530

    server file created with socket.io for chat app

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import cors from 'cors'
+import {Server} from 'socket.io'
+import http from 'http'
+
+
+
+const app = express()
+
+// create server using http
+
+const server = http.createServer(app)
+
+// create socket server
+
+const io = new Server(server,{
+    cors:{
+        origin:"*",
+        methods:['GET', "POST"]
+    }
+})
+
+
+// use socket events
+
+io.on('connection',(socket)=>{
+    console.log('Connection is establised');
+    Socket.on('disconnect',()=>{
+        console.log('Connection is disconected');
+    })
+})
+
+server.listen(3500,()=>{
+    console.log('Server is listening on 3500');
+})
